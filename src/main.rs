6ca9da76778d69c45e2cbb20201ae826428ce681
itsgerliz <gerworks.hs@gmail.com>
2025-02-14 10:33:10 +0100

    ft: argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+use env_logger::Env;
+use log::debug;
+
+#[derive(Parser)]
+#[command(version, about)]
+struct CliArgs {
+    #[arg(help = "Are we running as server?", short = 'l', long = "listening", required = false)]
+    listening: bool,
+    #[arg(help = "Address to connect/listen to/on", required = true)]
+    address: String,
+    #[arg(help = "Port to connect/listen to/on", required = true)]
+    port: u16
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Default logging level to INFO
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    
+    // Parse arguments
+    let args = CliArgs::parse();
+
+    debug!("Starting tcpChat...");
 }
